612c4dbfa9e051334dec9566538f11a4c27a689d
Wmissary <pro.wmissary@gmail.com>
2023-07-09 21:44:53 +0200

    chore create Banner component & style

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,10 @@
+import "../../css/Banner/style.css";
+
+export default function Banner({ image, text }) {
+  return (
+    <div className="banner">
+      <img src={image} alt="banner" className="banner_image" />
+      <p className="banner_text">{text}</p>
+    </div>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,10 @@
+import Banner from "../../components/Banner";
+import banner_home from "../../assets/banner_home.jpg";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main className="main">
+      <Banner image={banner_home} text="Chez vous, partout et ailleurs" />
+    </main>
   );
 }
