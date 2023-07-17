3bdc66abd8a1e835dd57c59ea3a8a36ca603c9e8
Wmissary <pro.wmissary@gmail.com>
2023-07-17 16:08:34 +0200

    chore: page 404 & style

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,7 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 export default function PageNotFound() {
   return (
-    <div>
-      <h1>Page Not Found</h1>
-    </div>
+    <main className="notfound">
+      <h1 className="notfound_title">404</h1>
+      <p className="notfound_text">Oups! La page que vous demandez n'existe pas.</p>
+      <NavLink to="/" className="notfound_link" end>
+        Retour à la page d'accueil
+      </NavLink>
+    </main>
   );
 }
