13cfd2a118bd292a99b45d54f26f30b4c78b0dc1
Wmissary <pro.wmissary@gmail.com>
2023-07-23 17:44:12 +0200

    chore: begin lodging css

--- src/pages/Lodging/index.jsx
+++ src/pages/Lodging/index.jsx
@@ -5,6 +5,8 @@ import data from "../../data/data.json";
 import Collapse from "../../components/Collapse";
 import Slideshow from "../../components/Slideshow";
 
+import "../../css/pages/Lodging/style.css";
+
 export default function Lodging() {
   const { id } = useParams();
   const lodging = data.find(lodging => lodging.id === id);
@@ -12,21 +14,25 @@ export default function Lodging() {
   return (
     <main className="lodging">
       <Slideshow pictures={lodging.pictures} />
-      <div className="lodging_informations">
-        <h1>{lodging.title}</h1>
-        <h2>{lodging.location}</h2>
-        <ul className="lodging_tags">
-          {lodging.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
-      <div className="lodging_host_informations">
-        <h3>{lodging.host.name}</h3>
-        <img src={lodging.host.picture} alt={lodging.host.name} />
-      </div>
-      <div className="lodging_rating">
-        <span>{lodging.rating}</span>
+      <div className="lodging_flex">
+        <div className="lodging_informations">
+          <h1>{lodging.title}</h1>
+          <h2>{lodging.location}</h2>
+          <ul className="lodging_tags">
+            {lodging.tags.map(tag => (
+              <li key={tag}>{tag}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="lodging_host_informations">
+          <div className="lodging_flex">
+            <h3>{lodging.host.name}</h3>
+            <img src={lodging.host.picture} alt={lodging.host.name} />
+          </div>
+          <div className="lodging_rating">
+            <span>{lodging.rating}</span>
+          </div>
+        </div>
       </div>
       <div className="lodging_collapses">
         <Collapse title="Description" text={lodging.description} />
